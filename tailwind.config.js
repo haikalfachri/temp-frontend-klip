8363abaa4e9ea94c7aa5f0ad981db8cbce7ade23
Muhamad Fachri Haikal <90541349+hklfach@users.noreply.github.com>
2022-11-13 02:22:16 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./MengelolaUser.html" , "./MengelolaProduk.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      poppins: "'Poppins', sans-serif",
+      lato: "'Lato', sans-serif",
+    }
+  },
+  plugins: [],
+}
